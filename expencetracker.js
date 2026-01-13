575c7f96fce21f079ac0b7e75fe8f01fa2852889
David Lazic <lazic.ddavid@gmail.com>
2026-01-13 14:39:55 +0100

    Added   height: 60%;

---
+++ expencetracker.js
@@ -0,0 +1,8 @@
+const menuItems = document.querySelectorAll(".category ul li");
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    menuItems.forEach((li) => li.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
