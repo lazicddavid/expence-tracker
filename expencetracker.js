d27030afe6b3491bb425dcbcc643dca54bd477e3
David Lazic <lazic.ddavid@gmail.com>
2026-01-15 22:17:12 +0100

    )

--- expencetracker.js
+++ expencetracker.js
@@ -7,11 +7,6 @@ menuItems.forEach((item) => {
   });
 });
 
-const menuItems = document.querySelectorAll(".category ul li");
-
-menuItems.forEach((item) => {
-  item.addEventListener("click", () => {
-    menuItems.forEach((li) => li.classList.remove("active"));
-    item.classList.add("active");
-  });
-});
+const DOM = {
+  incomes: document.querySelector(".incomes"),
+};
