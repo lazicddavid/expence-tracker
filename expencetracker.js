58b5bf17014034d74081a875e9bddb69f1f7469a
David Lazic <lazic.ddavid@gmail.com>
2026-01-16 23:13:32 +0100

    Added .balance-amount {   color: #8bc34a; }

--- expencetracker.js
+++ expencetracker.js
@@ -1,12 +1,17 @@
-const menuItems = document.querySelectorAll(".category ul li");
-
-menuItems.forEach((item) => {
-  item.addEventListener("click", () => {
-    menuItems.forEach((li) => li.classList.remove("active"));
-    item.classList.add("active");
-  });
-});
-
 const DOM = {
   incomes: document.querySelector(".incomes"),
+  menuItems: document.querySelectorAll(".category ul li"),
+  dashboardSection: document.querySelector(".incomes-section"),
 };
+
+DOM.menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    DOM.menuItems.forEach((li) => li.classList.remove("active"));
+    item.classList.add("active");
+
+    if (item.textContent.includes("incomes")) {
+      DOM.dashboardSection.classList.add("hidden");
+      DOM.dashboardSection.classList.remove("hidden");
+    }
+  });
+});
